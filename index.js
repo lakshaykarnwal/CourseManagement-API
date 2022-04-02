790b9404e9f7457640dac84e6db9ed50faa2011a
lakshaykarnwal <lakshaykarnwal@gmail.com>
2022-04-02 12:15:54 +0530

    Completed the api

--- index.js
+++ index.js
@@ -1,31 +1,90 @@
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const port = process.env.PORT || 8080; // using a enviornment port or 8080 (default)
 
-app.use(express.json())
+app.use(express.json()); //using middleware
 
-app.listen(
-    PORT, 
-    () => console.log(`The server is live on http://localhost:${PORT}`)
-)
+const courses = [
+    { id: 1, name: 'COSC'},
+    { id: 2, name: 'PHIL'},
+    { id: 3, name: 'ECON'}
+];
 
-app.get('/tshirt', (req, res) => {
-    res.status(200).send({
-        tshirt: '👕',
-        size: 'large'
-    })
+app.get('/', (req, res) => {
+    res.send("CourseManagement API - Manage your courses with ease");
 });
 
-app.post('/tshirt/:id', (req, res) => {
-    const { id } = req.params;
-    const { logo } = req.body;
+//Get request for all courses
+app.get('/api/courses', (req, res) => {
+    res.send(courses);
+});
 
-    if(!logo){
-        res.status(418).send({ message: 'We need a logo' })
+//Get request for a course based on its ID
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+
+    if(!course) res.status(404).send("Course with the given ID was not found")
+    res.send(course);
+});
+
+//Adds a new course
+app.post('/api/courses', (req, res) => {
+    // some error handling
+    if(!req.body.name || req.body.name.length < 3) {
+        //400 Bad Request
+        res.status(400).send('Name is required and should be minimum 3 characters.');
+        return;
     }
 
-    res.send({
-        tshirt: `👕 with your ${logo} and ID of ${id}`,
-    });
+    //Add the new course to the array
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+
+    //display the course
+    res.send(course);
 });
+
+//Adds a course based on the given ID
+app.put('/api/courses/:id', (req, res) => {
+    //Check if the course of the given ID exists
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+
+    if(!course) res.status(404).send("Course with the given ID was not found")
+    res.send(course);
+
+    // Check if the given name matches the required criteria
+    if(!req.body.name || req.body.name.length < 3) {
+        //400 Bad Request
+        res.status(400).send('Name is required and should be minimum 3 characters.');
+        return;
+    }
+
+    //update the course
+    course.name = req.body.name;
+    res.send(course);
+});
+
+//Deletes a course based on the given ID
+app.delete('/api/courses/:id', (req, res) => {
+    //Check if the course of the given ID exists
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+
+    if(!course) res.status(404).send("Course with the given ID was not found")
+    res.send(course);
+
+    //Update the courses array
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    //send the course
+    res.send(course);
+});
+
+app.listen(
+    port, 
+    () => console.log(`The server is live on http://localhost:${port}`)
+);
 
